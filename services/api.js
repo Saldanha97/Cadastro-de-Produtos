2eb7deeac98ad06e206c7ced4eec6ef135d2d0d9
Emanuel Saldanha <152982797+Saldanha97@users.noreply.github.com>
2025-11-28 08:13:14 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:3000",
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}` // ou onde estiver guardando o token
   }
