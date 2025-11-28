0ca4378e2468e3cc57b1de903d52f808936c76ef
Emanuel Saldanha <152982797+Saldanha97@users.noreply.github.com>
2025-11-28 16:27:02 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,20 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-   withCredentials: true
-
-});
-
-
-api.interceptors.request.use(config => {
-  const token = localStorage.setItem("token", token);
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
+    baseURL:import.meta.env.VITE_API_URL
+})
 
 
 export default api
